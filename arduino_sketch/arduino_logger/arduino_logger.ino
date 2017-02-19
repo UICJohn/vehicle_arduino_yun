f41486972065facfbda7b66ac6fd765041253988
John <johntang1026@gmail.com>
2017-02-19 23:39:51 +1030

    Add Fonts

--- arduino_sketch/arduino_logger/arduino_logger.ino
+++ arduino_sketch/arduino_logger/arduino_logger.ino
@@ -1,62 +1,85 @@
+#include <ILI9341_due_config.h>
+#include <ILI9341_due.h>
+#include "fonts/digit.h"
+#include "fonts/font.h"
 #include <Event.h>
 #include <Timer.h>
-
-#include <fonts.h>
-#include <UTFT.h>
-#include <memorysaver.h>
-#include <MultiLCD.h>
 
 #include <OBD2UART.h>
 #include <Process.h>
 #include <SoftwareSerial.h>
 #include "config.h"
-Timer t;
+
+#define TFT_CS 5 // Chip Select for LCD
+#define TFT_DC 6  // Command/Data for LCD
+#define TFT_RST 4 // Command/Data for LCD
+#define TFT_LED 7
+
+ILI9341_due tft = ILI9341_due(TFT_CS, TFT_DC, TFT_RST);
+//Timer t;
+
+gTextArea speedArea{ 40, 40, 240, 150 };
+int rpm = 1000;
 COBD obd;
-LCD_ILI9341 lcd; 
 void setup() {
-  Bridge.begin();
-  Console.begin();
   obd.begin();
+  tft.begin();
+  pinMode(TFT_LED, OUTPUT);
+  digitalWrite(TFT_LED, HIGH);
+  tft.setRotation(iliRotation270);
+  tft.fillScreen(ILI9341_WHITE);
+  lcdPrint("Welcome", 60, 90, 2);
+  delay(3000);
+  tft.clearTextArea();
+  digitalWrite(TFT_LED, LOW);
   while(!obd.init());
-  lcd.begin();
-  //Setup Timer For Data writing here
+  lcdPrint("km/h", 140, 170, 1);
+  if(errorExisted()){
+    obd.clearDTC(); 
+  }
+}
+
+void lcdPrint(String str,int x, int y, int scale){
+  tft.setTextColor(ILI9341_BLUE, ILI9341_WHITE);
+  tft.setFont(font);
+  tft.setTextScale(scale);
+  tft.printAt(str, x, y);
 }
 
 void loop(){
-  t.update();
+  readPID();
+  delay(50);
 }
 
-unsigned int insertToDB(String cols[], String value[], String table, int col_size){
-  Process p;
-  String cmd = "sqlite3 -line "+database +" ";
-  String columns_string = "'insert into " + table + " (";
-  String values_string = "Values (";
-  for(int i = 0 ;  i < col_size; i++) {
-    columns_string = columns_string + '\"' + cols[i] + '\"';
-    values_string = values_string + '\"'+value[i] + '\"';
-    if(i != (col_size-1)){
-      columns_string += ", ";
-      values_string += ", ";
-    }
+
+void printInfo(int sp){
+  tft.setTextArea(speedArea);
+  tft.setFont(digit);
+  tft.setTextScale(2);
+  if(sp >= 10 && sp <100){
+    tft.printAt(String(sp), 60, 20, 30, 30);
   }
-  cmd = cmd + columns_string + ") " +values_string + ")'";
-  p.runShellCommand(cmd);
-}
-
-void setMonitor(String info[]) {
-  lcd.setFontSize(FONT_SIZE_XLARGE);
-  lcd.print(info[0]);
-  lcd.setFontSize(FONT_SIZE_MEDIUM);
-  lcd.print("km/h");
-  
+  else if(sp >= 100){
+    tft.printAt(String(sp), 30+(sp-100)/10, 30, 30, 30);
+  }else{
+    tft.printAt(String(sp), 90, 20, 30, 30);
+  }
 }
 
 void readPID(){
-  static const byte pids[] = {};
+  static const byte pids[] = {PID_SPEED, PID_RPM};
   int values[sizeof(pids)];
-  if(obd.readPID(pids, sizeof(pids), values)){
-    // process pid  
+  if(obd.readPID(pids, sizeof(pids), values) == sizeof(pids)){
+    if(values[1] != 0){
+      digitalWrite(TFT_LED, HIGH); 
+    }
+    printInfo(values[0]);
+    rpm = values[1];
+  }else{
+    digitalWrite(TFT_LED, LOW);
+    while(!obd.init());
   }
+  
 }
 
 void processDTC(){
@@ -64,6 +87,14 @@ void processDTC(){
   if(obd.readDTC(codes, 6) != 0){
     //process DTC
   }
+}
+
+boolean errorExisted(){
+  unsigned int codes[6];
+  if(obd.readDTC(codes, 6) != 0){
+    return true;
+  }
+  return false;
 }
 
 void clearDTC(){
